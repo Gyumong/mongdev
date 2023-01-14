0e95976ca2f600f7762a8b199daa642a72c49fb9
aiden <rlaalsrb3559@naver.com>
2023-01-15 02:14:21 +0900

    config: fix next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-import { withContentlayer } from "next-contentlayer/.";
+const { withContentlayer } = require("next-contentlayer");
+
 const nextConfig = {
   reactStrictMode: true,
 };
