8f82ead5b97a084d0ecd856fb255d1ad2b5e06f1
aiden <rlaalsrb3559@naver.com>
2023-01-15 21:33:48 +0900

    feat: font 적용 및 커스텀

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,49 @@
 import Head from "next/head";
 import Link from "next/link";
 import { compareDesc, format, parseISO } from "date-fns";
-import { Inter } from "@next/font/google";
+import localFont from "@next/font/local";
 import { allPosts, Post } from "@/.contentlayer/generated";
 import { InferGetStaticPropsType } from "next";
 
-const inter = Inter({ subsets: ["latin"] });
+const SfProDisplayFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/SF-Pro-Display-Black.woff2",
+      weight: "900",
+    },
+    {
+      path: "../public/fonts/SF-Pro-Display-Bold.woff2",
+      weight: "700",
+    },
+    {
+      path: "../public/fonts/SF-Pro-Display-Medium.woff2",
+      weight: "500",
+    },
+  ],
+  variable: "--font-sfpro-display",
+});
+
+const SfProTextFont = localFont({
+  src: [
+    {
+      path: "../public/fonts/SF-Pro-Text-Bold.woff2",
+      weight: "700",
+    },
+    {
+      path: "../public/fonts/SF-Pro-Text-Medium.woff2",
+      weight: "500",
+    },
+    {
+      path: "../public/fonts/SF-Pro-Text-Light.woff2",
+      weight: "200",
+    },
+  ],
+  variable: "--font-sfpro-text",
+});
 
 function PostCard(post: Post) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col body-16">
       <time dateTime={post.date} className="text-sm text-slate-600">
         {format(parseISO(post.date), "LLLL d, yyyy")}
       </time>
@@ -26,12 +60,12 @@ export default function Home({
   posts,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div>
+    <div className={`${SfProDisplayFont.variable} ${SfProTextFont.variable}`}>
       <Head>
         <title>Contentlayer Blog Example</title>
       </Head>
 
-      <h1>Contentlayer Blog Example</h1>
+      <h1 className="title-26">Contentlayer Blog Example</h1>
 
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,57 @@
+const plugin = require("tailwindcss/plugin");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  jit: true,
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "Sf-display": [
+          "var(--font-sfpro-display)",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        "Sf-text": ["var(--font-sfpro-text)", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addComponents }) => {
+      addComponents({
+        ".body-14": {
+          fontFamily: "Sf-text",
+          fontSize: "0.875rem",
+          fontWeight: "500",
+          lineHeight: 1.43,
+        },
+        ".body-16": {
+          fontFamily: "Sf-text",
+          fontSize: "1rem",
+          fontWeight: "500",
+          lineHeight: 1.44,
+        },
+        ".title-22": {
+          fontFamily: "Sf-display",
+          fontSize: "1.375rem",
+          fontWeight: "700",
+          lineHeight: 1.36,
+        },
+        ".title-24": {
+          fontFamily: "Sf-display",
+          fontSize: "1.5rem",
+          fontWeight: "700",
+          lineHeight: 1.34,
+        },
+        ".title-26": {
+          fontFamily: "Sf-display",
+          fontSize: "1.625rem",
+          fontWeight: "700",
+          lineHeight: 1.3,
+        },
+      });
+    }),
+  ],
 };
