70a37b9ceec238955244834f5c20423606df0ecc
gyumong <gyumong@github.com>
2023-01-22 22:17:14 +0900

    feat: svgr 추가 및 svg icon 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const { withContentlayer } = require("next-contentlayer");
 
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
